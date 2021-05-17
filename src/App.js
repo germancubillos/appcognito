1b2245b5e2fc65e30d49fe336d9c5d0d6d4441e7
GermanCubillos <gyb0726@gmail.com>
2021-05-16 21:56:18 -0500

    Algunos labels es

--- src/App.js
+++ src/App.js
@@ -3,8 +3,54 @@ import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import awsconfig from './aws-exports';
-Amplify.configure(awsconfig);
+import { I18n } from 'aws-amplify';
 
+Amplify.configure(awsconfig);
+I18n.setLanguage('es');
+
+const dict = {
+  'fr': {
+      'Sign In': "Se connecter",
+      'Sign Up': "S'inscrire"
+  },
+  'es': {
+    'Sign In': 'Registrarse',
+    'Sign in to your account': 'Como me le va vecino',
+		'Sign Up': 'Regístrate',
+		'Sign Out': 'Desconectar',
+    'Username *': "Vecino escriba su nombre de usuario:",
+    'Password  *': "La contraseña de siempre",
+		'Forgot Password': 'Se te olvidó tu contraseña',
+		Username: 'Nombre de usuario',
+		Password: 'Contraseña',
+		'Change Password': 'Cambia la contraseña',
+		'New Password': 'Nueva contraseña',
+		Email: 'Email',
+		'Phone Number': 'Número de teléfono',
+		'Confirm a Code': 'Confirmar un código',
+		'Confirm Sign In': 'Confirmar inicio de sesión',
+		'Confirm Sign Up': 'Confirmar Registrarse',
+		'Back to Sign In': 'Volver a Iniciar sesión',
+		'Send Code': 'Enviar código',
+		Confirm: 'Confirmar',
+		'Resend a Code': 'Reenviar un código',
+		Submit: 'Enviar',
+		Skip: 'Omitir',
+		Verify: 'Verificar',
+		'Verify Contact': 'Verificar contacto',
+		Code: 'Código',
+		'Account recovery requires verified contact information':
+			'La recuperación de la cuenta requiere información de contacto verificada',
+
+		'User does not exist': 'el usuario no existe',
+		'User already exists': 'El usuario ya existe',
+		'Incorrect username or password':
+			'Nombre de usuario o contraseña incorrecta',
+		'Invalid password format': 'Formato de contraseña inválido'
+  }
+};
+
+I18n.putVocabularies(dict);
 
 function App() {
   return (
@@ -16,5 +62,6 @@ function App() {
     </div>
   );
 }
+
 
 export default withAuthenticator(App);
