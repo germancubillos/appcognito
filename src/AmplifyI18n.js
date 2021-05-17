0b86c81b40c5fe24e5f80da84f86d94ab8eccbe0
GermanCubillos <gyb0726@gmail.com>
2021-05-17 16:09:41 -0500

    Label es y clase I18n

---
+++ src/AmplifyI18n.js
@@ -0,0 +1,58 @@
+import { I18n } from 'aws-amplify';
+
+I18n.setLanguage('es');
+
+export const dict = {
+    'es': {
+      'Sign In': 'Registrarse',
+      'Sign in': 'Registrarse',
+      'Sign in to your account': "Paseo El Éden - Autenticación",
+      'Sign Up': 'Regístrate',
+      'Sign Out': 'Desconectar',
+      'Username *': "Vecino escriba su nombre de usuario:",
+      'Enter your username': 'Ingrese su usuario',
+      'Password *': "La contraseña de siempre",
+      'Enter your password':"Ingrese su contraseña",
+      'Reset password':"Cambie su contraseña",
+      'No account?':"No tienes una cuenta?",
+      'Create account':'Crea una cuenta',
+      'Create Account': 'Crea una cuenta',
+      '+1': '+57',
+      '(555)555-1212': '+57',
+          'Forgot your password?': 'Se te olvidó tu contraseña',
+          Username: 'Nombre de usuario',
+          Password: 'Contraseña',
+          'Change Password': 'Cambia la contraseña',
+          'New Password': 'Nueva contraseña',
+          Email: 'Email',
+          'Phone Number': 'Número de teléfono',
+          'Confirm a Code': 'Confirmar un código',
+          'Confirm Sign In': 'Confirmar inicio de sesión',
+          'Confirm Sign Up': 'Confirmar Registrarse',
+          'Back to Sign In': 'Volver a Iniciar sesión',
+          'Send Code': 'Enviar código',
+          Confirm: 'Confirmar',
+          'Resend a Code': 'Reenviar un código',
+          Submit: 'Enviar',
+          Skip: 'Omitir',
+          Verify: 'Verificar',
+          'Verify Contact': 'Verificar contacto',
+          Code: 'Código',
+          'Account recovery requires verified contact information':
+          'La recuperación de la cuenta requiere información de contacto verificada',
+          'User does not exist': 'el usuario no existe',
+          'User already exists': 'El usuario ya existe',
+          'Incorrect username or password':
+          'Nombre de usuario o contraseña incorrecta',
+          'Invalid password format': 'Formato de contraseña inválido',
+      'Reset your password': 'Cambie su contraseña',
+      'Verification code': 'Código de verificación',
+      'Enter code': 'Ingrese código',
+      'New password': 'Nueva contraseña',
+      'Enter your new password': 'Ingrese contraseña',
+      'Create a new account': 'Crea una nueva cuenta',
+      'Email Address *': 'Dirección de correo electronico',
+      'Phone Number *': 'Número de telefono',
+      'Have an account?': 'Tienes ya una cuenta?'
+    }
+  };
--- src/App.js
+++ src/App.js
@@ -3,55 +3,10 @@ import './App.css';
 import Amplify, { Auth } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import awsconfig from './aws-exports';
-import { I18n } from 'aws-amplify';
+import { I18n } from '@aws-amplify/core';
+import { dict } from './AmplifyI18n';
 
 Amplify.configure(awsconfig);
-I18n.setLanguage('es');
-
-const dict = {
-  'fr': {
-      'Sign In': "Se connecter",
-      'Sign Up': "S'inscrire"
-  },
-  'es': {
-    'Sign In': 'Registrarse',
-    'Sign in to your account': 'Como me le va vecino',
-		'Sign Up': 'Regístrate',
-		'Sign Out': 'Desconectar',
-    'Username *': "Vecino escriba su nombre de usuario:",
-    'Password  *': "La contraseña de siempre",
-		'Forgot Password': 'Se te olvidó tu contraseña',
-		Username: 'Nombre de usuario',
-		Password: 'Contraseña',
-		'Change Password': 'Cambia la contraseña',
-		'New Password': 'Nueva contraseña',
-		Email: 'Email',
-		'Phone Number': 'Número de teléfono',
-		'Confirm a Code': 'Confirmar un código',
-		'Confirm Sign In': 'Confirmar inicio de sesión',
-		'Confirm Sign Up': 'Confirmar Registrarse',
-		'Back to Sign In': 'Volver a Iniciar sesión',
-		'Send Code': 'Enviar código',
-		Confirm: 'Confirmar',
-		'Resend a Code': 'Reenviar un código',
-		Submit: 'Enviar',
-		Skip: 'Omitir',
-		Verify: 'Verificar',
-		'Verify Contact': 'Verificar contacto',
-		Code: 'Código',
-		'Account recovery requires verified contact information':
-			'La recuperación de la cuenta requiere información de contacto verificada',
-
-		'User does not exist': 'el usuario no existe',
-		'User already exists': 'El usuario ya existe',
-		'Incorrect username or password':
-			'Nombre de usuario o contraseña incorrecta',
-		'Invalid password format': 'Formato de contraseña inválido'
-  }
-};
-
-I18n.putVocabularies(dict);
-
 function App() {
   return (
     <div className="App">
@@ -63,5 +18,6 @@ function App() {
   );
 }
 
+I18n.putVocabularies(dict);
 
 export default withAuthenticator(App);
